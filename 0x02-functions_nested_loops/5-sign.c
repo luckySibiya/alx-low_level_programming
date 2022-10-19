0dba4d0f904e060985082f98880292b4dcfa84ad
Lucky Sibiya <lm.sibiya28@gmail.com>
2022-10-18 19:31:15 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- * @c: the number of a sign to be printed
+ * @n: the number of a sign to be printed
  * Return: 1 if the number is greater than zero
  * 0 of number is zero
  *-1 if the number is less than zero
